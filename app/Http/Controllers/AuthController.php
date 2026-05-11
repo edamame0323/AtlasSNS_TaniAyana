c43136e36c443c849742295ee904908895facf4d
taynwrite0248 <tayn.write0248@gmail.com>
2026-05-11 22:48:44 +0900

    バリデーションメッセージを日本語にした。　登録完了をregister_complete.blade.phpからadded.blade.phpにした。

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -21,7 +21,31 @@ class AuthController extends Controller
             'username' => 'required|min:2|max:12',
             'email' => 'required|email|min:5|max:40|unique:users',
             'password' => 'required|alpha_num|min:8|max:20|confirmed',
-        ]);
+        ],
+        [
+            'username.required' => 'ユーザー名を入力してください',
+            'username.min' => 'ユーザー名は2文字以上で入力してください',
+            'username.max' => 'ユーザー名は12文字以内で入力してください',
+
+            'email.required' => 'メールアドレスを入力してください',
+            'email.email' => 'メールアドレス形式で入力してください',
+            'email.min' => 'メールアドレスは5文字以上で入力してください',
+            'email.max' => 'メールアドレスは40文字以内で入力してください',
+            'email.unique' => 'このメールアドレスは既に登録されています',
+
+            'password.required' => 'パスワードを入力してください',
+            'password.alpha_num' => 'パスワードは英数字のみで入力してください',
+            'password.min' => 'パスワードは8文字以上で入力してください',
+            'password.max' => 'パスワードは20文字以内で入力してください',
+            'password.confirmed' => '確認用パスワードが一致しません',
+        ]
+        );
+
+        // ＊ポイント＊ 第2因数が「日本語メッセージ」
+        // validate(
+        //     条件,
+        //     エラーメッセージ
+        // );
 
         $user = User::create([
             'username' => $request->username,
@@ -30,7 +54,7 @@ class AuthController extends Controller
         ]);
 
         // 完了画面にユーザー情報を渡す
-        return view('auth.register_complete', ['user' => $user]);
+        return view('auth.added', ['user' => $user]);
     }
 
     public function showLoginForm()
